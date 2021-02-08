1c5ca2aa043e9a9cb4c59debb3342a99aefe3e49
almog ram <almoghr36@gmail.com>
2021-02-08 12:03:46 +0200

    built a new Agents app and now arrangin views, urls and configurations for it.

---
+++ agents/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import generic
+from leads.models import Agent
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+    def get_queryset(self):
+        return Agent.objects.all()
