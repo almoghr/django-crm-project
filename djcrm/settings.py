74e50ef10e8b4f45ff065f21f0aedf23ea3f567e
almog ram <almoghr36@gmail.com>
2021-02-08 11:55:14 +0200

    added redirections + urls for auth, added userProfile + model adding while using signals, added custom usercreation form, added a minor test just for learning, added, signup method, views and functionality, made a few changes to html FE files

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT = "static_root"
 AUTH_USER_MODEL = 'leads.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_REDIRECT_URL = '/leads'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
