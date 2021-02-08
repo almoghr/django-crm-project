bf48673c2e3702e569e2cf9b00704d0e14fd33c8
almog ram <almoghr36@gmail.com>
2021-02-08 10:23:08 +0200

    added styling to some html views, replaced the views with class based, established local emailing simple sys

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render, redirect
-#from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
+from django.views import generic
+
+class LandingPageView(generic.TemplateView):
+    template_name = 'landing_page.html'
+class LeadListView(generic.ListView):
+    template_name = 'leads/lead_list.html'
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
+class LeadDetailView(generic.DetailView):
+    template_name = 'leads/lead_detail.html'
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
+class LeadCreateView(generic.CreateView):
+    template_name = 'leads/lead_create.html'
+    form_class = LeadModelForm
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+    def form_valid(self, form):
+        send_mail(
+            subject="a lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
+
+class LeadUpdateView(generic.UpdateView):
+    template_name = 'leads/lead_update.html'
+    form_class = LeadModelForm
+    queryset = Lead.objects.all()
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+class LeadDeleteView(generic.DeleteView):
+    template_name = 'leads/lead_delete.html'
+    queryset = Lead.objects.all()
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
 
 def lead_list(request):
     leads=Lead.objects.all()
